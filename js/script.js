645bbc09159fd0002801386b231ecbb111510ac4
SerenaMalusa <sere.malu@gmail.com>
2024-01-29 14:44:41 +0100

    feat: add images with v-for

--- js/script.js
+++ js/script.js
@@ -3,8 +3,13 @@ const { createApp } = Vue;
 createApp ({
     data() {
         return {
-            text : 'ciao da me',
-            imgUrl : 'https://www.breatheazy.co.uk/wp-content/uploads/2023/09/Untitled-design-35-1080x675.png'
+            text : `Un po' di gatti per rallegrare la vostra giornata ;)`,
+            imgsUrls : [
+                'https://www.breatheazy.co.uk/wp-content/uploads/2023/09/Untitled-design-35-1080x675.png',
+                './img/cat-christmas-tree-lights.jpg',
+                'https://mcdn.wallpapersafari.com/medium/6/0/jxu8wX.jpg',
+                'https://images5.alphacoders.com/287/287909.jpg'
+            ]
         }
     }
 }).mount('#app')
