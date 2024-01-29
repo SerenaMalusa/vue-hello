bd9964610d1f515753379c9027dbf085bb718b7d
SerenaMalusa <sere.malu@gmail.com>
2024-01-29 14:30:03 +0100

    feat: add image trough Vue: data

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 createApp ({
     data() {
         return {
-            text : 'ciao da me'
+            text : 'ciao da me',
+            imgUrl : 'https://www.breatheazy.co.uk/wp-content/uploads/2023/09/Untitled-design-35-1080x675.png'
         }
     }
 }).mount('#app')
